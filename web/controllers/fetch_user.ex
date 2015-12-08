5d083e14d6cfcb1262b14207b72e5ecfd6ee8a07
Troels Brødsgaard <tr@arbr.dk>
2015-12-08 10:45:02 +0100

    refactor FetchUser

--- web/controllers/fetch_user.ex
+++ web/controllers/fetch_user.ex
@@ -4,8 +4,11 @@ defmodule MagpiePresenter.FetchUser do
   def init(opts), do: nil
 
   def call(conn, _) do
-    user_id = get_session(conn, :user_id)
-    user = user_id && Magpie.DataAccess.User.get(user_id)
-    assign(conn, :current_user, user)
+    case get_session(conn, :user_id) do
+      nil -> assign(conn, :current_user, nil)
+      user_id ->
+        {:ok, user} = Magpie.DataAccess.User.get(user_id)
+        assign(conn, :current_user, user)
+    end
   end
 end
