885a52b2eb67e174eb83ba31d378b535063d5968
mikkel <desmaagraa@gmail.com>
2015-12-11 15:27:08 +0100

    add visual

--- web/controllers/sensor_controller.ex
+++ web/controllers/sensor_controller.ex
@@ -23,10 +23,9 @@ defmodule MagpiePresenter.SensorController do
     {:ok, from} = DateFormat.parse(params["from"], "{0M}/{0D}/{YYYY}{h24}:{m}:{s}")
     {:ok, to} = DateFormat.parse(params["to"], "{0M}/{0D}/{YYYY}{h24}:{m}:{s}")
 
-    Timex.Date.set(from, [hour: 0, minute: 0, second: 0, ms: 0, validate: false])
-    Timex.Date.set(to, [hour: 0, minute: 0, second: 0, ms: 0, validate: false])
+    date = Timex.Date.set(from, [hour: 0, minute: 0, second: 0, ms: 0, validate: false])
 
-    measurements = Magpie.DataAccess.Measurement.get(params["id"], from, from, to)
+    measurements = Magpie.DataAccess.Measurement.get(params["id"], date, from, to)
     data = Enum.reduce(measurements,"", fn(m, acc) ->
       acc <> "#{m[:timestamp]},#{m[:value]}\n"
     end)
