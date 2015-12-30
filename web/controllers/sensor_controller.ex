d0a35c2cfa1123196fab9c9eb28b7af6c54bd009
Troels Brødsgaard <tr@arbr.dk>
2015-12-30 13:04:36 +0100

    fix query across days

--- web/controllers/sensor_controller.ex
+++ web/controllers/sensor_controller.ex
@@ -3,7 +3,6 @@ defmodule MagpiePresenter.SensorController do
   use Timex
 
   def batch(conn, params) do
-    # TODO: if no date given, it returns {:error, "message"}
     {:ok, sensor} = Magpie.DataAccess.Sensor.get(params["id"], params["logger_id"])
     conn
     |> assign(:sensor, sensor)
@@ -11,7 +10,6 @@ defmodule MagpiePresenter.SensorController do
   end
 
   def live(conn, params) do
-    # TODO: if no date given, it returns {:error, "message"}
     {:ok, sensor} = Magpie.DataAccess.Sensor.get(params["id"], params["logger_id"])
     
     conn
@@ -20,12 +18,11 @@ defmodule MagpiePresenter.SensorController do
   end
 
   def get_data(conn, params) do
-    {:ok, from} = DateFormat.parse(params["from"], "{0M}/{0D}/{YYYY}{h24}:{m}:{s}")
-    {:ok, to} = DateFormat.parse(params["to"], "{0M}/{0D}/{YYYY}{h24}:{m}:{s}")
+    {:ok, from} = DateFormat.parse(params["from"], "{0M}/{0D}/{YYYY} {h24}:{m}:{s}")
+    {:ok, to} = DateFormat.parse(params["to"], "{0M}/{0D}/{YYYY} {h24}:{m}:{s}")
 
-    date = Timex.Date.set(from, [hour: 0, minute: 0, second: 0, ms: 0, validate: false])
-
-    measurements = Magpie.DataAccess.Measurement.get(params["id"], date, from, to)
+    # TODO: if no date given, it returns {:error, "message"}
+    measurements = Magpie.DataAccess.Measurement.get(params["id"], from, to)
     data = Enum.reduce(measurements,"", fn(m, acc) ->
       acc <> "#{m[:timestamp]},#{m[:value]}\n"
     end)
