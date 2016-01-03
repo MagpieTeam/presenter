7e710c088674829db8f52a757e70172e815fb9f1
mikkel <desmaagraa@gmail.com>
2016-01-03 11:38:57 +0100

    update get_data

--- web/controllers/sensor_controller.ex
+++ web/controllers/sensor_controller.ex
@@ -21,8 +21,23 @@ defmodule MagpiePresenter.SensorController do
     {:ok, from} = DateFormat.parse(params["from"], "{0M}/{0D}/{YYYY} {h24}:{m}:{s}")
     {:ok, to} = DateFormat.parse(params["to"], "{0M}/{0D}/{YYYY} {h24}:{m}:{s}")
 
+    limit_seconds = Date.subtract(to, Time.to_timestamp(166, :minutes))
+    limit_minutes = Date.subtract(to, Time.to_timestamp(166, :hours))
+
+    resolution = 
+      case Date.diff(from, to, :secs) do
+        x when x < 10000 ->
+          :seconds
+        x when x < 600000 ->    
+          :minutes
+        _ ->
+          :hours
+
+
+
+
     # TODO: if no date given, it returns {:error, "message"}
-    measurements = Magpie.DataAccess.Measurement.get(params["id"], from, to)
+    measurements = Magpie.DataAccess.Measurement.get(params["id"], from, to, resolution)
     data = Enum.reduce(measurements,"", fn(m, acc) ->
       acc <> "#{m[:timestamp]},#{m[:value]}\n"
     end)
